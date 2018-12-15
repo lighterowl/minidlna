d297624e533d6d4274c750e3769749c3d7f121c8
Daniel Kamil Kozar <dkk089@gmail.com>
2018-12-15 16:34:30 +0100

    Mark all instances of magic_container_s as const

--- upnpsoap.c
+++ upnpsoap.c
@@ -800,7 +800,7 @@ add_res(char *size, char *duration, char *bitrate, char *sampleFrequency,
 }
 
 static int
-get_child_count(const char *object, struct magic_container_s *magic)
+get_child_count(const char *object, const struct magic_container_s *magic)
 {
 	int ret;
 
@@ -1292,7 +1292,7 @@ BrowseContentDirectory(struct upnphttp * h, const char * action)
 			"<Result>"
 			"&lt;DIDL-Lite"
 			CONTENT_DIRECTORY_SCHEMAS;
-	struct magic_container_s *magic;
+	const struct magic_container_s *magic;
 	char *zErrMsg = NULL;
 	char *sql, *ptr;
 	struct Response args;
@@ -1807,7 +1807,7 @@ SearchContentDirectory(struct upnphttp * h, const char * action)
 			"<Result>"
 			"&lt;DIDL-Lite"
 			CONTENT_DIRECTORY_SCHEMAS;
-	struct magic_container_s *magic;
+	const struct magic_container_s *magic;
 	char *zErrMsg = NULL;
 	char *sql, *ptr;
 	struct Response args;
